0c0bc2147eeb5969de5ae1fbb7078bef1f778481
ARadhitama <ariqradhitamaa@gmail.com>
2019-03-21 14:48:42 +0700

    Tes2

--- Player.java
+++ Player.java
@@ -1,1 +1,13 @@
-kuyf
+public class Player {
+    private String name;
+    private int money;
+    private int pos;
+    private Boolean inJail;
+    
+    public Player (String name) {
+        this.name = name;
+        this.money = 0;
+        this.inJail = false;
+        this.pos = 0;
+    }
+}
