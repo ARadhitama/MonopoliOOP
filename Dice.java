7cf812b4d35c3fcfd90385d9454cb8a46753e7e7
ARadhitama <ariqradhitamaa@gmail.com>
2019-03-22 16:28:11 +0700

    class dadu, penggunaan di main

---
+++ Dice.java
@@ -0,0 +1,16 @@
+import java.util.random;
+
+public class Dice {
+    Random randomGen = new Random();
+    private int count = 0;
+    private int sides;
+
+    public Dice (int numSides) {
+        this.sides = numSides;
+    }
+
+    public int roll () {
+        int result = randomGen.nextInt(sides) + 1;
+        return result;
+    }
+}
