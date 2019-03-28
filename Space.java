d6e0fa97ec31695caf305f0865b98d7e9d8d5b99
MohammadNathiq13 <ndaskentangmnu@gmail.com>
2019-03-28 22:26:32 +0700

    apdet lagi cok

--- Space.java
+++ Space.java
@@ -2,7 +2,7 @@ import java.util.Random;
 
 public class Space extends Tile {
     Random randomGen = new Random();
-    private int sides;
+    private int cards = 5;
 
     
     public void startTile(Player P){                                //jika posisi pada startTile 
@@ -32,53 +32,73 @@ public class Space extends Tile {
     }
 
     public void communityChestCard(Player P){
-        int result = randomGen.nextInt(sides);
-        switch(sides) {
+        int result = randomGen.nextInt(cards);
+        switch(result) {
             case 1 :
                 System.out.println("Melanggar marka jalan, Anda dilempar ke penjara!");
                 P.setPos(10);
                 P.setJail(true);
+                break;
             case 2 :
                 System.out.println("Anda menang togel!! dapat Rp.100.000,-");
                 P.addMoney(100000);
+                break;
             case 3 :
                 System.out.println("Vaksinisasi wajib!! bayar Rp.15.000");
                 P.rdcMoney(15000);
+                break;
             case 4 :
                 System.out.println("Sekolah itu gak gratis!! ga bayar sekolah RP.50.000 PKI!!!!!");
                 P.rdcMoney(15000);
+                break;
             case 5 :
                 System.out.println("Cie menang olimpiade, selamat anda mendapatkan Rp.50.000");
-                P.addMoney(50000);    
+                P.addMoney(50000);
+                break;    
         } 
             
     }
 
     public void chanceCard(Player P){
-        int result = randomGen.nextInt(sides);
-        switch(sides) {
+        int result = randomGen.nextInt(cards);
+        switch(result) {
             case 1 : 
                 System.out.println("Keciduk polisi, hiya hiya hiya, korupsi ya?? masuk penjara sini!! jangan korupsi dong");
                 P.setPos(10);
                 P.setJail(true);
+                break;
             case 2 :
                 System.out.println("Pergi ke go!! lumayan dapet Rp.40.000");
                 P.setPos(0);
                 this.startTile(P);
+                break;
             case 3 :
                 System.out.println("Maju 3 langkah dong");
-                P.setPos(P.getpos()+3);
+                P.setPos(P.getPos()+3);
+                break;
             case 4 :
                 System.out.println("Astaghfirullah menang judi.... tapi gaapa uang monopoli kokga haram");
                 P.addMoney(100000);
+                break;
             case 5 : 
                 System.out.println("Mundur 3 langkah");
                 getPos(P);
-                setpos(getpos(P)-3);
+                setPos(getPos(P)-3);
+                break;
         }
     }
 
     public boolean landedMethod(Player p){
-
+        if (P.getPos() == 0){
+            this.startTile(P);
+        } else if (P.getPos()==30){
+            this.goToJailTile(P);
+        } else if (P.getPos()==20){
+            this.freeParkingTile(P);
+        } else if (P.getPos()==2 || P.getPos()==17 || P.getPos()==32){
+            this.communityChestCard(P);
+        } else if (P.getPos()==7 || P.getPos()==22 || P.getPos()==36){
+            this.chanceCard(P);
+        }
     }
 }
