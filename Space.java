d44e7710e4990f74ac90c4ead596b952bd03f451
MohammadNathiq13 <ndaskentangmnu@gmail.com>
2019-03-28 22:11:44 +0700

    apdet cok

--- Space.java
+++ Space.java
@@ -1,16 +1,16 @@
 import java.util.Random;
 
 public class Space extends Tile {
-    Random randomGen = new Random;
+    Random randomGen = new Random();
     private int sides;
 
     
-    public void startTile(int pos){                                //jika posisi pada startTile 
+    public void startTile(Player P){                                //jika posisi pada startTile 
                                                                    //akan menambahkna 2x uang start
-        if (pos == 0){
-            money += 400;
+        if (P.getPos() == 0){
+            P.addMoney(40000);
         } else {
-            money += 200;
+            P.addMoney(20000);
         }
     }
 
@@ -18,9 +18,11 @@ public class Space extends Tile {
         
     }
 
-    public void freeParkingTile(int pos){
-        if (pos == 20) {
-            System.out.println("mau mendarat dimana? : "+ )
+    public void freeParkingTile(Player P){
+        
+        if (P.getPos()==20) {
+            System.out.println("mau mendarat dimana? : ");
+            P.setPos(sc.nextInt());
         } 
     }
 
@@ -29,7 +31,7 @@ public class Space extends Tile {
         P.setJail(true);
     }
 
-    public void communityChestCard(){
+    public void communityChestCard(Player P){
         int result = randomGen.nextInt(sides);
         switch(sides) {
             case 1 :
@@ -46,24 +48,37 @@ public class Space extends Tile {
                 System.out.println("Sekolah itu gak gratis!! ga bayar sekolah RP.50.000 PKI!!!!!");
                 P.rdcMoney(15000);
             case 5 :
-                System.out.println("Cie menang olimpiade, selamat anda mendapatkan Rp.50.000")
-                P.addMoney(50000)    
+                System.out.println("Cie menang olimpiade, selamat anda mendapatkan Rp.50.000");
+                P.addMoney(50000);    
         } 
             
     }
 
-    public void chanceCard(){
+    public void chanceCard(Player P){
         int result = randomGen.nextInt(sides);
         switch(sides) {
             case 1 : 
                 System.out.println("Keciduk polisi, hiya hiya hiya, korupsi ya?? masuk penjara sini!! jangan korupsi dong");
-                P.setPos(9);
+                P.setPos(10);
                 P.setJail(true);
             case 2 :
                 System.out.println("Pergi ke go!! lumayan dapet Rp.40.000");
                 P.setPos(0);
-                startTile();
+                this.startTile(P);
             case 3 :
-                System.out.println("");
+                System.out.println("Maju 3 langkah dong");
+                P.setPos(P.getpos()+3);
+            case 4 :
+                System.out.println("Astaghfirullah menang judi.... tapi gaapa uang monopoli kokga haram");
+                P.addMoney(100000);
+            case 5 : 
+                System.out.println("Mundur 3 langkah");
+                getPos(P);
+                setpos(getpos(P)-3);
+        }
+    }
+
+    public boolean landedMethod(Player p){
+
     }
 }
